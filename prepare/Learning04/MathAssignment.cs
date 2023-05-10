c7f98480cc43ac0215de209d8689403c81b68296
mikkikaye <mckenziej5@gmail.com>
2023-05-09 22:52:32 -0600

    did learni6ng #4

---
+++ prepare/Learning04/MathAssignment.cs
@@ -0,0 +1,25 @@
+using System;
+
+public class MathAssignment : Assignment
+{
+    private string _textbookSection;
+    private string _problems;
+    public MathAssignment(string textbookSection, string problems, string studentName, string topic)
+        : base(studentName, topic)
+    {
+        _textbookSection = textbookSection;
+        _problems = problems;
+    }
+    public string GetTextbookSection()
+    {
+        return _textbookSection;
+    }
+    public string GetProblems()
+    {
+        return _problems;
+    }
+    public string GetHomeworkList()
+    {
+        return $"Section {_textbookSection} Problems {_problems}";
+    }
+}
--- prepare/Learning04/Program.cs
+++ prepare/Learning04/Program.cs
@@ -4,6 +4,18 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Learning04 World!");
+        Assignment assignment = new Assignment("Fred Weasley", "Numbers & Words");
+        string print = assignment.GetSummary();
+        Console.WriteLine(print);
+
+        MathAssignment mathAssignment = new MathAssignment( "7.3", "8-19", "Fred Weasley", "Math");
+        string printSecond = mathAssignment.GetHomeworkList();
+        Console.WriteLine(printSecond);
+
+        WritingAssignment writingAssignment = new WritingAssignment("Mary Waters", "European History", "The Causes of WWII");
+        string printThird = writingAssignment.GetSummary();
+        Console.WriteLine(printThird);
+        string printFourth = writingAssignment.GetWritingInformation();
+        Console.WriteLine(printFourth);
     }
 }
